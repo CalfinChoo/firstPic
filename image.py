2a54f7b2926da9b53b9bc11e4e99d78cb1bfa5df
Calvin Chu <cchu00@stuy.edu>
2020-02-01 00:24:50 -0500

    done

--- image.py
+++ image.py
@@ -3,13 +3,25 @@ width = 496
 height = 496
 f.write("P3\n{} {}\n255\n".format(width, height))
 
-sqSize = width/8
+sqSize = int(width/8)
 swap = False
+targets = [(133, 94, 66), (222, 184, 135)]
+
+def fold(val):
+    if (val > width / 2): val = width - val
+    return val
+
+def calcColor(x, y):
+    return int((width/2 - (fold(x) + fold(y)) / 2) - 66)
+
 for y in range(height):
     for x in range(width):
-        for i in range(3):
-            if (swap): f.write("222 184 135 ")
-            else: f.write("180 207 250 ")
-        if (x % sqSize == 0 and swap == False): swap = True
-        elif (x % sqSize == 0 and swap == True): swap = False
+        if (swap): f.write("{} {} {} ".format(str(targets[0][0] + calcColor(x, y)), str(targets[0][1] + calcColor(x, y)), str(targets[0][2] + calcColor(x, y))))
+        else: f.write("{} {} {} ".format(str(targets[1][0] + calcColor(x, y)), str(targets[1][1] + calcColor(x, y)), str(targets[1][2] + calcColor(x, y))))
+        if ((x + 1) % sqSize == 0 and swap == False): swap = True
+        elif ((x + 1) % sqSize == 0 and swap == True): swap = False
+    if ((y + 1) % sqSize == 0 and swap == False): swap = True
+    elif ((y + 1) % sqSize == 0 and swap == True): swap = False
     f.write("\n")
+f.close()
+#Draws a gradient chessboard
